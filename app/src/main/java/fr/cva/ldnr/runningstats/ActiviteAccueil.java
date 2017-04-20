5f662937a1e19c6c8fe97097cef8be1c66b7c3ad
AinsR <ainsley.remy@gmail.com>
2017-04-20 11:24:31 +0200

    menu dans l'activité accueil, modif parametres et manifest

--- app/src/main/java/fr/cva/ldnr/runningstats/ActiviteAccueil.java
+++ app/src/main/java/fr/cva/ldnr/runningstats/ActiviteAccueil.java
@@ -1,8 +1,12 @@
 package fr.cva.ldnr.runningstats;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by Ains on 20/04/2017.
@@ -15,5 +19,47 @@ public class ActiviteAccueil extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.general, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featuredId, MenuItem item){
+        switch(item.getItemId()) {
+            case R.id.menu_enregistrer:
+                Intent intent = new Intent(this, ActiviteAjout.class);
+                startActivity(intent);
+                return  true;
+            case R.id.menu_historique:
+                Intent intent2 = new Intent(this, ActiviteHistorique.class);
+                startActivity(intent2);
+                return  true;
+            case R.id.menu_statistiques:
+                Intent intent3 = new Intent(this, ActiviteStats.class);
+                startActivity(intent3);
+                return  true;
+            case R.id.menu_parametres:
+                Intent intent4 = new Intent(this, ActiviteParam.class);
+                startActivity(intent4);
+                return  true;
+            default:
+                return false;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 
 }
