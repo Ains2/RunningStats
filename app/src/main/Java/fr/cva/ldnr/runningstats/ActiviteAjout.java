18a8ef563b035088b2b5c71836fe20e76cf708e6
cbessou <claire.bessou@laposte.net>
2017-04-20 11:15:07 +0200

    Merge branch 'master' of C:\Users\Claire\AndroidStudioProjects\RunningStats with conflicts.

--- app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteAjout.java
+++ app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteAjout.java
@@ -1,10 +1,17 @@
 package fr.cva.ldnr.runningstats;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by Nanwee on 19/04/2017.
  */
 
 public class ActiviteAjout extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ajout);
+    }
 }
