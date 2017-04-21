4b08d93219894652a76375d264403efb297ce846
cbessou <claire.bessou@laposte.net>
2017-04-21 23:25:53 +0200

    Affichage graphique - correction bug quand pas de données

--- app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteGraph.java
+++ app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteGraph.java
@@ -1,12 +1,16 @@
 package fr.cva.ldnr.runningstats;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -22,42 +26,84 @@ import fr.cva.ldnr.runningstats.GestionDonnees.GestionBDD;
  * Created by Nanwee on 19/04/2017.
  */
 
-public class ActiviteGraph extends fr.cva.ldnr.runningstats.Menu  {
+public class ActiviteGraph extends fr.cva.ldnr.runningstats.Menu {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.graphique);
-        LineChart chart = (LineChart) findViewById(R.id.chart);
 
-        GestionBDD gbdd = GestionBDD.getInstance(this);
-        /* correspond à la requete: "SELECT dates, temps FROM sprint WHERE dist = 100 ORDER BY dates DESC LIMIT 10"*/
-        String[] tab = {"_id", "dates", "dist", "temps"};
-        String[] args = {"100"};
-        Cursor c = gbdd.selectSprint(tab, "dist=?", args, null, null, "dates ASC", null);
+        //Récupération du Spinner de la liste des distances
+        Spinner list_dist = (Spinner) findViewById(R.id.selectdist);
+        //Création de la liste des distances
+        List<String> list = new ArrayList<>();
+        list.add("60");
+        list.add("100");
+        list.add("200");
+        list.add("400");
 
-        List<Entry> entries = new ArrayList<>();
-        int i=0;
-        for (c.moveToFirst();!c.isAfterLast();c.moveToNext()) {
-            // turn your data into Entry objects
-            entries.add(new Entry(i, c.getFloat(3)));
-            i++;
-        }
-        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-        dataSet.setColor(R.style.button);
-        dataSet.setValueTextColor(R.style.button); // styling, ...
+        // Création de l'adaptater pour afficher le contenu
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                list
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        list_dist.setAdapter(adapter);
+        list_dist.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv = (TextView) view;
+                String spin_result = (String) tv.getText();
+                showgraph(spin_result);
+            }
 
-        LineData lineData = new LineData(dataSet);
-        chart.setData(lineData);
-        chart.invalidate(); // refresh
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.i("ActiviteGraph", parent.toString());
+            }
+        });
     }
 
-    public void seehisto (View view) {
+    public void showgraph(String dist) {
+        GestionBDD gbdd = GestionBDD.getInstance(this);
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+        try {
+            String[] tab = {"_id", "dates", "dist", "temps"};
+            String[] args = {dist};
+            Log.i("ActiviteGraph", "debut try");
+            /* correspond à la requete: "SELECT dates, temps FROM sprint WHERE dist = 100 ORDER BY dates ASC"*/
+            Cursor c = gbdd.selectSprint(tab, "dist=?", args, null, null, "dates ASC", null);
+            Log.i("ActiviteGraph", c.toString());
+            List<Entry> entries = new ArrayList<>();
+            if (c.moveToFirst()) {
+                int i = 0;
+                for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+                    // turn your data into Entry objects
+                    entries.add(new Entry(i, c.getFloat(3)));
+                    i++;
+                }
+                c.close();
+
+                LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+                dataSet.setColor(Color.BLUE);
+                dataSet.setValueTextColor(Color.WHITE); // styling, ...
+
+                LineData lineData = new LineData(dataSet);
+                chart.setData(lineData);
+                chart.invalidate(); // refresh
+            }else{
+                chart.setData(null);
+                chart.setNoDataText(getString(R.string.no_data_text));
+                chart.invalidate(); // refresh
+            }
+        } catch (Exception e) {
+            Log.e("ActiviteGraph", e.getMessage());
+        }
+    }
+
+    public void seehisto(View view) {
         Intent intent = new Intent(this, ActiviteHistorique.class);
         startActivity(intent);
     }
-
-
-
-
 }
