fe270a8634175a5725df0384d635908cfd02a89d
AinsR <ainsley.remy@gmail.com>
2017-04-21 09:45:51 +0200

    mise à jour

--- app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteGraph.java
+++ app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteGraph.java
@@ -1,10 +1,29 @@
 package fr.cva.ldnr.runningstats;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 
 /**
  * Created by Nanwee on 19/04/2017.
  */
 
-public class ActiviteGraph extends Activity {
+public class ActiviteGraph extends fr.cva.ldnr.runningstats.Menu  {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.graphique);
+    }
+
+    public void seehisto (View view) {
+        Intent intent = new Intent(this, ActiviteHistorique.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
