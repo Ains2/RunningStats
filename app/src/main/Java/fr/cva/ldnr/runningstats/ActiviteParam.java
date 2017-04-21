7e1e0bffecbadba44899899ccdff0359f2e3b112
AinsR <ainsley.remy@gmail.com>
2017-04-21 18:49:40 +0200

    mise à jour

--- app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteParam.java
+++ app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteParam.java
@@ -3,59 +3,70 @@ package fr.cva.ldnr.runningstats;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.mikephil.charting.data.Entry;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.FilterWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.cva.ldnr.runningstats.GestionDonnees.GestionBDD;
 
 /**
  * Created by Nanwee on 19/04/2017.
  */
 
-public class ActiviteParam extends Activity {
-
-
+public class ActiviteParam extends fr.cva.ldnr.runningstats.Menu {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.parametres);
     }
 
+    public void parametres_export(View view) {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.general, menu);
-        return true;
-    }
+            try {
+                GestionBDD gbdd = GestionBDD.getInstance(this);
+                String[] tab = {"*"};
+                Cursor c = gbdd.selectSprint(tab, null, new String[0], null, null, "_id DESC", null);
+                c.moveToFirst();
 
+                File dir = Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DOCUMENTS);
 
+                dir.mkdirs();
 
+                File f = new File(dir, "export.xml");
+                FileWriter fw = new FileWriter(f, true);
 
+                for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+                    // Log.i("GestionBDD", "_id=" + c.getInt(0) + " dist=" + c.getInt(1) + " temps=" + c.getDouble(2) + " dates=" + c.getString(3) + " compet=" + c.getInt(4) + " nom=" + c.getString(5) + " classement=" + c.getInt(6));
+                    fw.write("_id=" + c.getInt(0) + " dist=" + c.getInt(1) + " temps=" + c.getDouble(2) + " dates=" + c.getString(3) + " compet=" + c.getInt(4) + " nom=" + c.getString(5) + " classement=" + c.getInt(6));
+                }
 
-    @Override
-    public boolean onMenuItemSelected(int featuredId, MenuItem item){
-        switch(item.getItemId()) {
-            case R.id.menu_enregistrer:
-                Intent intent = new Intent(this, ActiviteAjout.class);
-                startActivity(intent);
-                return  true;
-            case R.id.menu_historique:
-                Intent intent2 = new Intent(this, ActiviteHistorique.class);
-                startActivity(intent2);
-                return  true;
-            case R.id.menu_statistiques:
-                Intent intent3 = new Intent(this, ActiviteStats.class);
-                startActivity(intent3);
-                return  true;
+                fw.close();
+                c.close();
 
-            default:
-                return false;
+            } catch (IOException ex) {
+                Log.e("param", "pb support");
+            }
         }
-
     }
-
-
-
 }
