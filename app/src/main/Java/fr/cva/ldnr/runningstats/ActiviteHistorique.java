5149bf65382b6670baeca578223990d8b8a489d9
AinsR <ainsley.remy@gmail.com>
2017-04-21 15:38:11 +0200

    mise à jour

--- app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteHistorique.java
+++ app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteHistorique.java
@@ -1,6 +1,7 @@
 package fr.cva.ldnr.runningstats;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.MatrixCursor;
@@ -9,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TableLayout;
@@ -31,36 +33,47 @@ public class ActiviteHistorique extends fr.cva.ldnr.runningstats.Menu {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.historique);
 
-        Log.i("hist", "coucou");
-
         GestionBDD gbdd = GestionBDD.getInstance(this);
-        /*Cursor c = gbdd.selectSprint("SELECT dates, dist, temps FROM sprint ORDER BY dates DESC LIMIT 10");*/
-        String [] tab= {"_id", "dates","dist"};
-        Cursor c = gbdd.selectSprint(tab, null,new String[0], null, null,"dates DESC", "10");
-        Log.i("h",c.getColumnCount()+" "+c.getCount());
-        /*for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
-                String date =c.getString(0);
-                int dist = c.getInt(1);
-                double temps= c.getDouble(2);
-                Log.i("Historique",date+" : "+dist + "m en " + temps+"s");
-            }*/
-            // on prendra les données des colonnes 1 et 2...
-            String[] from = new String[] {"dates", "dist"};
+        /* correspond à la requete: "SELECT dates, dist, temps FROM sprint ORDER BY dates DESC LIMIT 10"*/
+        String[] tab = {"_id", "dates", "dist", "temps"};
+        Cursor c = gbdd.selectSprint(tab, null, new String[0], null, null, "dates DESC", "10");
 
-            // ...pour les placer dans les TextView définis dans "row_item.xml"
-            int[] to = new int[] { R.id.textViewCol1, R.id.textViewCol2};
+        // on prendra les données des colonnes 1 et 2...
+        String[] from = new String[]{"dates", "dist", "temps"};
 
-            //try {
-                // création de l'objet SimpleCursorAdapter...
-                SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.row_item, c, from, to, 0);
-                // ...qui va remplir l'objet ListView
-                ListView lv = (ListView) findViewById(R.id.lv);
-                lv.setAdapter(adapter);
+        // ...pour les placer dans les TextView définis dans "row_item.xml"
+        int[] to = new int[]{R.id.textViewCol1, R.id.textViewCol2, R.id.textViewCol3};
 
-            //} finally {
-            //c.close();
+        //try {
+        // création de l'objet SimpleCursorAdapter...
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.row_item, c, from, to, 0);
+        // ...qui va remplir l'objet ListView
+        ListView lv = (ListView) findViewById(R.id.lv);
+        lv.setAdapter(adapter);
+
+        //} finally {
+        //c.close();
+
+
+
+        /*
+        // Gestion des clics sur les lignes
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View container, int position, long id) {
+                // faites ici ce que vous voulez
+                Log.i("mydebug","clic sur id:"+id);
+
+            }
+        };
+
+    // Utilisation avec notre listview
+        lv.setOnItemClickListener(itemClickListener);
+    }*/
+
 
     }
+
 
     public void gotoGraphique (View view) {
         Intent intent = new Intent(this, ActiviteGraph.class);
