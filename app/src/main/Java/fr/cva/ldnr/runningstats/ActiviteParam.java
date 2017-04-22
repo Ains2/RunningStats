7fd8cd38d23d5e4a3f7b8bc00f552cf34c56b880
AinsR <ainsley.remy@gmail.com>
2017-04-22 16:41:01 +0200

    mise à jour

--- app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteParam.java
+++ app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteParam.java
@@ -6,9 +6,12 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.text.Layout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,6 +19,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
 
 import fr.cva.ldnr.runningstats.GestionDonnees.GestionBDD;
 
@@ -54,4 +60,35 @@ public class ActiviteParam extends fr.cva.ldnr.runningstats.Menu {
         Intent intent = new Intent(this, ActiviteParam.class);
         startActivity(intent);
     }
+
+    public void parametres_export(View view) {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+
+            try {
+                GestionBDD gbdd = GestionBDD.getInstance(this);
+                String[] tab = {"*"};
+                Cursor c = gbdd.selectSprint(tab, null, new String[0], null, null, "_id DESC", null);
+                c.moveToFirst();
+
+                File dir = Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_DOWNLOADS);
+
+                dir.mkdirs();
+
+                File f = new File(dir, "export.csv");
+                FileWriter fw = new FileWriter(f, true);
+
+                for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+                    // Log.i("GestionBDD", "_id=" + c.getInt(0) + " dist=" + c.getInt(1) + " temps=" + c.getDouble(2) + " dates=" + c.getString(3) + " compet=" + c.getInt(4) + " nom=" + c.getString(5) + " classement=" + c.getInt(6));
+                    fw.write("_id=" + c.getInt(0) + " dist=" + c.getInt(1) + " temps=" + c.getDouble(2) + " dates=" + c.getString(3) + " compet=" + c.getInt(4) + " nom=" + c.getString(5) + " classement=" + c.getInt(6));
+                }
+                fw.close();
+                c.close();
+
+            } catch (Exception ex) {
+                Log.e("param", "pb support"+ex.getMessage());
+            }
+        }
+    }
+
 }
