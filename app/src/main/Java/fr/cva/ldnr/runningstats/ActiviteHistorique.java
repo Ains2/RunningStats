0d4a3acc88355cec98358be14a276a0b603e4086
cbessou <claire.bessou@laposte.net>
2017-04-23 00:33:08 +0200

    close curseur historique + remis onclick param export

--- app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteHistorique.java
+++ app/src/main/Java/fr/cva/ldnr/runningstats/ActiviteHistorique.java
@@ -1,24 +1,12 @@
 package fr.cva.ldnr.runningstats;
 
-import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-
-import java.text.ParseException;
-import java.util.Date;
 
 import fr.cva.ldnr.runningstats.GestionDonnees.GestionBDD;
 
@@ -28,6 +16,8 @@ import fr.cva.ldnr.runningstats.GestionDonnees.GestionBDD;
 
 public class ActiviteHistorique extends fr.cva.ldnr.runningstats.Menu {
 
+    private Cursor cur_histo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +26,7 @@ public class ActiviteHistorique extends fr.cva.ldnr.runningstats.Menu {
         GestionBDD gbdd = GestionBDD.getInstance(this);
         /* correspond à la requete: "SELECT dates, dist, temps FROM sprint ORDER BY dates DESC LIMIT 10"*/
         String[] tab = {"_id", "dates", "dist", "temps"};
-        Cursor c = gbdd.selectSprint(tab, null, new String[0], null, null, "dates DESC", "10");
+        cur_histo = gbdd.selectSprint(tab, null, new String[0], null, null, "dates DESC", "10");
 
         // on prendra les données des colonnes 1 et 2...
         String[] from = new String[]{"dates", "dist", "temps"};
@@ -46,7 +36,7 @@ public class ActiviteHistorique extends fr.cva.ldnr.runningstats.Menu {
 
         //try {
         // création de l'objet SimpleCursorAdapter...
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.row_item, c, from, to, 0);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.row_item, cur_histo, from, to, 0);
         // ...qui va remplir l'objet ListView
         ListView lv = (ListView) findViewById(R.id.lv);
         lv.setAdapter(adapter);
@@ -74,6 +64,11 @@ public class ActiviteHistorique extends fr.cva.ldnr.runningstats.Menu {
 
     }
 
+    @Override
+    protected void onDestroy() {
+        cur_histo.close();
+        super.onDestroy();
+    }
 
     public void gotoGraphique (View view) {
         Intent intent = new Intent(this, ActiviteGraph.class);
